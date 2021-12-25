2018218b07c313ac500b9b8dbc8ff0402c35019c
bujosa <davidbujosa@gmail.com>
2021-12-25 10:21:05 +0100

    Add common listen

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const { PubSub } = require("@google-cloud/pubsub");
 const pubsub = new PubSub();
 const app = express();
 require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
