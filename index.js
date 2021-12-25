50ee504e17edc5cc9b8d79f296621166e27481e0
bujosa <davidbujosa@gmail.com>
2021-12-25 15:01:08 +0100

    Eliminated spacing

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const { PubSub } = require("@google-cloud/pubsub");
-
 const pubsub = new PubSub();
-
 const app = express();
-
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
