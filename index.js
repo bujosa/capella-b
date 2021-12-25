fdc2154c73c69a75651abe43dcaac160039e47b4
bujosa <davidbujosa@gmail.com>
2021-12-25 13:06:20 +0100

    Add credential config

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { PubSub } = require("@google-cloud/pubsub");
 
 // If you dont have key file, you have provided apiEndpoint and projectId.
 const pubsub = new PubSub({
-  apiEndpoint: process.env.PUB_SUB_API_ENDPOINT,
-  projectId: process.env.PUB_SUB_PROJECT_ID,
+  projectId: process.env.PUBSUB_PROJECT_ID,
 });
+
 const app = express();
+
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +21,7 @@ app.listen(port, () => {
 
 app.post("/api/example", async (req, res) => {
   try {
+    console.log("Project id", process.env.PUBSUB_PROJECT_ID);
     const data = req.body;
     await publishPubSubMessage("welcome-message", data);
   } catch (e) {
@@ -30,5 +32,8 @@ app.post("/api/example", async (req, res) => {
 
 async function publishPubSubMessage(topicName, data) {
   const dataBuffer = Buffer.from(JSON.stringify(data));
-  await pubsub.topic(topicName).publishMessage(dataBuffer);
+  //   console.log("dataBuffer", dataBuffer);
+  await pubsub.topic(topicName).publishMessage({
+    data: dataBuffer,
+  });
 }
