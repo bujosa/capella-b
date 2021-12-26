49b76020030997b1df56df59311ab304047d11a6
bujosa <davidbujosa@gmail.com>
2021-12-26 16:19:18 +0100

    Finish index example

--- index.js
+++ index.js
@@ -1,59 +1,11 @@
 const Server = require("./models/server");
+const Subscriptions = require("./models/subscriptions");
 
 require("dotenv").config();
 
 const server = new Server();
-
 server.execute();
 
-// app.post("/api/topic2", async (req, res) => {
-//   try {
-//     const data = req.body;
-
-//     // Publish message to PubSub
-//     await publishPubSubMessage("testing-topic-2", data);
-
-//     // Response status code
-//     res.status(200).send("Message sent to PubSub");
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).send(e);
-//   }
-// });
-
-// // Create my own subscriptions
-// async function createSubscription(
-//   topicName = "YOUR_TOPIC_NAME",
-//   subscriptionName = "YOUR_SUBSCRIPTION_NAME"
-// ) {
-//   // Creates a new subscription
-//   const [subscription] = await pubsub
-//     .topic(topicName)
-//     .createSubscription(subscriptionName);
-//   console.log(`Subscription ${subscriptionName} created.`);
-//   subscription.on("message", (message) => {
-//     console.log(
-//       "Received message from the topic testing-topic-2:",
-//       message.data.toString()
-//     );
-//     process.exit(0);
-//   });
-//   return subscription;
-// }
-
-// createTopic("testing-topic-1");
-
-// // Maybe the subscription name is the name of the your services
-// // Example: createSubscription("created-user", "wallet-service");
-// const capella_a = createSubscription("testing-topic-2", "capella-b").catch(
-//   console.error
-// );
-
-// Receive callbacks for new messages on the subscription
-// capella_a.on("message", (message) => {
-//   console.log(
-//     "Received message from the topic testing-topic-2:",
-//     message.data.toString()
-//   );
-//   process.exit(0);
-// });
+const subscriptions = new Subscriptions();
+subscriptions.initialize();
+subscriptions.execute();
