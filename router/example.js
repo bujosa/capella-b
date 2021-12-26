e615d19a7eebdf9e94c6cef94a0d5506aab1706d
bujosa <davidbujosa@gmail.com>
2021-12-26 15:40:07 +0100

    Create my first router of this project

---
+++ router/example.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { sendMessageToWelcomeMessage } = require("../controllers/example");
+const { validateJWT } = require("../middlewares/validate-jwt");
+const { validateFields } = require("../middlewares/validate-fields");
+
+const router = Router();
+
+// Example publish message to the topic
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Email is required").isEmail(),
+    validateFields,
+  ],
+  sendMessageToWelcomeMessage
+);
+
+// router.post("/topic2", [], createUser);
+
+// Example validate JWT
+// router.get("/renew", validateJWT, renewToken);
+
+module.exports = router;
+
+/* 
+path: api/example
+*/
